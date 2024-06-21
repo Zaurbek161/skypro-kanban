106df7723f20aa63be36df9e97e6a12854671ad1
Zaurbek <zaurcheg07@gmail.com>
2024-06-21 16:33:53 +0300

    hw-1

---
+++ vite-project/src/components/Column/index.jsx
@@ -0,0 +1,10 @@
+export const Column = ({ title, children }) => {
+  return (
+    <div className="main__column column">
+      <div className="column__title">
+        <p>{title}</p>
+      </div>
+      <div className="cards">{children}</div>
+    </div>
+  );
+};
